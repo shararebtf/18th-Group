1cc8f6e3841a63fe2256944afeb127806c666957
shararebtf <sharare.bitarafan@gmail.com>
2024-03-10 18:32:48 +0330

    factorial

---
+++ shararebtf/6th-week/factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 0:
+        print("Factorial is not defined for negative numbers")
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+factorial(2)
